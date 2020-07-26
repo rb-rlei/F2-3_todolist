8801ceaa208e5c2ea9b1b758b54b406202ad16b8
rb-rlei <heimulei85@gmail.com>
2020-07-26 20:02:42 +0800

    feat: add todo seeder

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const Todo = require('../todo')
+
+// mongoose 的連線設定
+mongoose.connect('mongodb://localhost/todo-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// 取得連線狀態參數
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: `name-${i}` })
+  }
+
+  console.log('done.')
+})
