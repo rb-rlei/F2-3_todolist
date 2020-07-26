b6174a955716cd617f48e4cf84f58c214bc200b4
rb-rlei <heimulei85@gmail.com>
2020-07-26 19:53:45 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
+
+// mongoose 的連線設定
+mongoose.connect('mongodb://localhost/todo-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// 取得連線狀態參數
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello world')
